5c32220aca52526695e347d81d2740aeece3839f
djshow832 <zhangming@pingcap.com>
2024-06-30 17:27:29 +0800

    add waiting

--- pkg/proxy/event/event.go
+++ pkg/proxy/event/event.go
@@ -6,6 +6,8 @@ import (
 	"sync"
 	"sync/atomic"
 	"time"
+
+	"github.com/pingcap/tiproxy/pkg/util/monotime"
 )
 
 var MyEvent *Event
@@ -57,16 +59,52 @@ func (e *Event) Start(ctx context.Context) {
 	}()
 	conn, _ := net.Dial("tcp", listener.Addr().String())
 	defer conn.Close()
+	ticker := time.NewTicker(40 * time.Microsecond)
+	defer ticker.Stop()
+	data := make([]byte, 1)
+	lastTime := monotime.Time(0)
+	for ctx.Err() == nil {
+		total := e.total.Load()
+		waiting := e.waiting.Load()
+		now := monotime.Now()
+		if waiting > total/2 || time.Duration(now-lastTime) > 400*time.Microsecond {
+			ch <- []byte{1}
+			conn.Read(data)
+			e.cond.Broadcast()
+			lastTime = now
+		}
+		select {
+		case <-ctx.Done():
+			return
+		case <-ticker.C:
+		}
+	}
+}
+
+func (e *Event) waitWithRead(ctx context.Context) {
+	listener, _ := net.Listen("tcp", "localhost:0")
+	defer listener.Close()
+	ch := make(chan []byte, 1)
+	go func() {
+		conn, _ := listener.Accept()
+		defer conn.Close()
+		for ctx.Err() == nil {
+			data := <-ch
+			conn.Write(data)
+		}
+	}()
+	conn, _ := net.Dial("tcp", listener.Addr().String())
+	defer conn.Close()
 	ticker := time.NewTicker(20 * time.Microsecond)
 	defer ticker.Stop()
 	data := make([]byte, 1)
 	for ctx.Err() == nil {
-		// waiting := e.waiting.Load()
-		// if waiting > 0 {
-		ch <- []byte{1}
-		conn.Read(data)
-		e.cond.Broadcast()
-		// }
+		waiting := e.waiting.Load()
+		if waiting > 0 {
+			ch <- []byte{1}
+			conn.Read(data)
+			e.cond.Broadcast()
+		}
 		select {
 		case <-ctx.Done():
 			return
