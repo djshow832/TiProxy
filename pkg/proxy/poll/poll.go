8d1774d13c165a66754d08a07e32ae199b9a2813
djshow832 <zhangming@pingcap.com>
2024-08-17 18:22:20 +0800

    use pool

--- pkg/proxy/poll/poll.go
+++ pkg/proxy/poll/poll.go
@@ -4,21 +4,31 @@
 package poll
 
 import (
+	"context"
 	"net"
 	"time"
 
 	"github.com/cloudwego/netpoll"
+	"github.com/pingcap/tiproxy/lib/util/waitgroup"
 )
 
 const (
 	CtxKeyClientConn = "client_conn"
+
+	goPoolSize = 100
+	goMaxIdle  = time.Minute
 )
+
+var wgp = waitgroup.NewWaitGroupPool(goPoolSize, goMaxIdle)
 
 func CreateListener(network, address string) (netpoll.Listener, error) {
 	return netpoll.CreateListener(network, address)
 }
 
 func Run(listener net.Listener, onConnect netpoll.OnConnect, onRequest netpoll.OnRequest, onDisconnect netpoll.OnDisconnect) error {
+	netpoll.SetRunner(func(ctx context.Context, f func()) {
+		wgp.RunWithRecover(f, nil, nil)
+	})
 	eventLoop, err := netpoll.NewEventLoop(
 		onRequest,
 		netpoll.WithOnConnect(onConnect),
